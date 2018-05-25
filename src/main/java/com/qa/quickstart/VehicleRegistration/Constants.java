2c09c62172262bf55f64db0799778726f025349d
brassrabbit <george.hardy@hotmail.co.uk>
2018-05-25 13:04:59 +0100

    first commit

---
+++ src/main/java/com/qa/quickstart/VehicleRegistration/Constants.java
@@ -0,0 +1,22 @@
+package com.qa.quickstart.VehicleRegistration;
+
+public class Constants {
+	
+	public static final String websiteURL = "http://thedemosite.co.uk";
+
+	public static final String registerURL = "http://thedemosite.co.uk/addauser.php";
+
+	public static final String loginURL = "http://thedemosite.co.uk/login.php";
+	
+	public static final String filePath = "C:\\WebDev\\Files\\";
+	
+	public static final String fileData = "DemoSiteDDT.xlsx";
+	
+	public static final String extentReportLocation = "C:\\WebDev\\Reports\\DemoSite.html";
+	
+	public static final String chromeDriver = "webdriver.chrome.driver";
+	
+	public static final String chromeDriverLocation = "C:\\WebDev\\Drivers\\chromedriver.exe";
+	 
+
+}
---
+++ src/main/java/vehicle-registration.feature
@@ -0,0 +1,10 @@
+Feature:
+As a user
+I want to search for vehicle registrations
+So that I can check that the details are correct
+
+Scenario:
+Given I go to the correct site
+When I navigate to the search page
+And I enter car details and search
+Then The correct details are shown and recorded 
